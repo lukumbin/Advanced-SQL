fda9bc5c7fd3d944a87282584dd1e6f7858bd589
Richard Lukumbi Ndlovu <111294158+lukumbin@users.noreply.github.com>
2025-11-20 08:12:42 +0200

    Add SQL queries for sales and customer analysis
    Added various SQL queries for data analysis and reporting.

--- Advanced SQL Concepts.sql
+++ Advanced SQL Concepts.sql
@@ -128,3 +128,103 @@ WITH RECURSIVE cte AS ( SELECT CAST('2025-01-01' AS DATE) AS dt
 SELECT	cte.dt,
         COALESCE(sales.num_sales, ROUND((LAG(sales.num_sales) OVER() + LEAD(sales.num_sales) OVER())/2)) AS sales_estimate
 FROM	cte LEFT JOIN sales ON cte.dt = sales.dt;
+
+
+
+
+select 
+orderid, creationtime, '2025-09-07' as hardcoded, NOW() AS TODAY
+from  orders;
+
+select 
+orderid, year(creationtime), monthname(creationtime), day(creationtime)
+from  orders;
+
+select
+orderid, creationtime, dayname(creationtime), cast(creationtime as date)
+from orders;
+
+select current_timestamp();   -- gives current date & time
+
+select current_time();   -- gives current time 
+
+select 3.516,
+round(3.516, 2) as round_2,     -- rounds to 2 decimal places
+round(3.516, 1) as round_1,    -- rounds to 1 decimal places
+round(3.516, 0) as round_0;   -- rounds to 0 decimal places
+
+select -10, 
+ABS(-10);      -- converts negative to positive numbers
+
+SELECT 
+now(),
+date_add('2025-05-29', interval 5 day) date_add,    -- adding 5 days on current date 
+date_sub(now(), interval 5 month) date_sub;    -- subtracting 5 months on current date 
+
+select 
+timestampdiff('2025-08-07','2010-05-05');     -- difference in days between two days 
+
+select 
+employeeid,birthdate,
+timestampdiff(year, birthdate, curdate()) as age   -- shwoing current age 
+from employees;
+ 
+ 
+select customerid, 
+score, avg(score) over() AVG_SCORE,              -- showing averages using window functions 
+avg(coalesce(Score,0)) over () AvgScore2 
+from customers;
+        					
+select 
+orderdate, date_format(orderdate),
+case when isdate(orderdate) = 1 THEN CAST(orderdate as date)
+end newOrderDate
+from orders;
+
+select distinct productid, sum(sales) over (partition by productid)        --  sum of sales group by products 
+from orders;
+
+
+select orderID, 
+orderdate, sales, rank () over (order by sales desc) as rank_sales    -- ranking sales from hoghest to lowest 
+from orders;
+
+
+Select 
+category,
+sum(sales) as total_Sales
+FROM ( 
+	Select sales,
+	CASE when sales > 50 then 'high'
+	when sales > 20 then 'medium'        -- report showing total sales categorized and sorted descedenly 
+    else 'low' 
+end category
+from orders
+)t
+group by category;
+
+
+select         
+customerid,                 -- average score of customer and treat null as 0 
+lastname, 
+score, 
+Case 
+	when score is null then 0 
+	else score 
+end score_clean,
+AVG (Case 
+		when score is null then 0 
+		else score 
+        end) over () AvgCustomerClean,
+avg(score) over() AvgCustomer
+from customers;
+
+
+select 
+customerid, 
+Sum(case                           -- count how many times each customer has an order with sales > 30
+	when sales > 30 then 1 
+    else 0 
+end) total_order
+from orders
+group by customerid;
